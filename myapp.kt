7f048a05844397992ee70d689d6d76446ef8d009
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-08-25 16:22:30 +0900

    hello world

---
+++ myapp.kt
@@ -0,0 +1,13 @@
+/*
+実行手順
+➜  kotlin_lessons kotlinc myapp.kt -include-runtime -d myapp.jar
+➜  kotlin_lessons ls
+myapp.jar myapp.kt
+➜  kotlin_lessons java -jar myapp.jar
+Hello world
+➜  kotlin_lessons
+*/
+
+fun main(args: Array<String>) {
+  println("Hello world")
+}
